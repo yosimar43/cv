eebce7bec7f1ffa3096affbe6ddeb8b6c1f11e15
Yosimar Sinisterra <michaelyosimar43@gmail.com>
2021-11-11 14:36:15 -0500

    optimize font

--- pages/_document.js
+++ pages/_document.js
@@ -10,18 +10,12 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap"
             rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-            integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Hind:wght@700&family=Lato&display=swap"
-            rel="stylesheet"
-          />
+          />{" "}
         </Head>
         <body>
           <Main />
